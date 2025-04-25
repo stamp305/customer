d2c2bc11a7a9247ad7f63b0008b3218f5a61274a
stamp305 <puripaht2016@gmail.com>
2025-04-25 19:50:47 +0700

    Update customer_segmentation.py

--- customer_segmentation.py
+++ customer_segmentation.py
@@ -34,7 +34,7 @@ X = data[features]
 
 # Clustering
 st.subheader("🧠 K-Means Clustering")
-k = 5  # fixed number of clusters
+k = st.slider("Select number of clusters (k)", 2, 10, 5)
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
